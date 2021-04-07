30e3ed06ab4da2e93282d57c045ebbf23a96756b
fillopfy <abhi456tm@gmail.com>
2021-04-08 00:07:40 +0530

    trying git staging area of intelliJ

--- src/main/java/com/fillopfy/rest/project/ProjectApplication.java
+++ src/main/java/com/fillopfy/rest/project/ProjectApplication.java
@@ -2,7 +2,7 @@ package com.fillopfy.rest.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//trying git staging area of intellij
 @SpringBootApplication
 public class ProjectApplication {
 

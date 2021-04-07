7a6dd0b821ae04102ccc39aa2ef49abf7818cf9a
fillopfy <abhi456tm@gmail.com>
2021-04-07 23:25:30 +0530

    Initial set up

---
+++ src/test/java/com/fillopfy/rest/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fillopfy.rest.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
